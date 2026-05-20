dbad5315409100de955d46a6fe991f71e7ee8c0c
Ellie Larson <ellie@polklarson.com>
2026-05-20 00:09:38 -0500

    try fixing deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  }
+]
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
